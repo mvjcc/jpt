59a9f4baaba2e5bfaddbd2d0b404d33dc56acbee
Mario Victor Jose Cabanag <mvjccabanag@gmail.com>
2019-07-08 17:10:59 +0800

    + Contact class
    + basic test for Contact

---
+++ app/models/Contact.java
@@ -0,0 +1,62 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+ 
+import play.db.jpa.*;
+ 
+@Entity
+public class Contact extends Model {
+
+	public String fname;			// val="First Name"
+	public String lname;			// val="Last Name"
+	public String email;			// val="Email"
+	public String title;			// val="Title"
+	public String phone;			// val="Phone"
+	public String mobile;			// val="Mobile"
+//	public String fullname;			// val="Full Name"
+	public String mailingStreet;	// val="Mailing Street"
+	public String mailingCity;		// val="Mailing City"
+	public String mailingState;		// val="Mailing State"
+	public String mailingZip;		// val="Mailing Zip"
+	public String mailingCountry;	// val="Mailing Country"
+	public boolean emailOptOut;		// val="Email Opt Out"
+	public String skypeId;			// val="Skype ID"
+	public String lastActivityTime;	// val="Last Activity Time"
+	public String twitter;			// val="Twitter"
+
+	public Contact(
+			String fname,
+			String lname,
+			String email,
+			String title,
+			String phone,
+			String mobile,
+			String mailingStreet,
+			String mailingCity,
+			String mailingState,
+			String mailingZip,
+			String mailingCountry,
+			boolean emailOptOut,
+			String skypeId,
+			String lastActivityTime,
+			String twitter
+			) {
+		this.fname = fname;
+		this.lname = lname;
+		this.email = email;
+		this.title = title;
+		this.phone = phone;
+		this.mobile = mobile;
+		this.mailingStreet = mailingStreet;
+		this.mailingCity = mailingCity;
+		this.mailingState = mailingState;
+		this.mailingZip = mailingZip;
+		this.mailingCountry = mailingCountry;
+		this.emailOptOut = emailOptOut;
+		this.skypeId = skypeId;
+		this.lastActivityTime = lastActivityTime;
+		this.twitter = twitter;
+	}
+
+}
--- test/BasicTest.java
+++ test/BasicTest.java
@@ -7,7 +7,17 @@ public class BasicTest extends UnitTest {
 
     @Test
     public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
+    	// Create a new user and save it
+        new Contact("Sage", "Wieser", "sage-wieser@truhlar.uk", "Product Engineer", "555-555-5555", "555-555-5555", "5 Boston Ave #88",
+        		"Sioux Falls", "SD", "57105", "United States",
+        		false, "sage_wieser", "2019-07-08 14:44:29", "@sagewieser").save();
+        
+        // Retrieve the user with e-mail address bob@gmail.com
+        Contact sage = Contact.find("byEmail", "sage-wieser@truhlar.uk").first();
+        
+        // Test 
+        assertNotNull(sage);
+        assertEquals("Sage", sage.fname);
     }
 
 }
